14834b7be92e0262ad79ca5dd0fce4ce55fad1b7
Alex Poulos <alex@equul.us>
2021-04-15 09:38:15 -0400

    adding public

--- ddb_store.go
+++ ddb_store.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"os"
+	"strconv"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
@@ -15,12 +16,14 @@ type MeditationRecord struct {
 	Pk         string     `dynamodbav:"pk"`
 	Sk         string     `dynamodbav:"sk"`
 	Ppk        string     `dynamodbav:"ppk"`
+	Pppk       string     `dynamodbav:"pppk"`
 	Meditation Meditation `dynamodbav:"meditation"`
 }
 
 type MeditationStore interface {
 	SaveMeditation(m Meditation) error
 	ListMeditations(userId string) ([]Meditation, error)
+	ListPublicMeditations(userId string) ([]Meditation, error)
 	GetMeditation(userId string, id string) (Meditation, error)
 	DeleteMeditation(userId string, id string) error
 	UpdateMeditation(m Meditation) error
@@ -43,6 +46,7 @@ func createDynamoTable(tableName string, svc *dynamodb.DynamoDB) {
 			{AttributeName: aws.String("pk"), AttributeType: aws.String("S")},
 			{AttributeName: aws.String("sk"), AttributeType: aws.String("S")},
 			{AttributeName: aws.String("ppk"), AttributeType: aws.String("S")},
+			{AttributeName: aws.String("pppk"), AttributeType: aws.String("S")},
 		},
 		BillingMode: aws.String(dynamodb.BillingModePayPerRequest),
 		GlobalSecondaryIndexes: []*dynamodb.GlobalSecondaryIndex{
@@ -50,6 +54,16 @@ func createDynamoTable(tableName string, svc *dynamodb.DynamoDB) {
 				IndexName: aws.String("gs1"),
 				KeySchema: []*dynamodb.KeySchemaElement{
 					{AttributeName: aws.String("ppk"), KeyType: aws.String("HASH")},
+				},
+				Projection: &dynamodb.Projection{
+					ProjectionType: aws.String(dynamodb.ProjectionTypeAll),
+				},
+			},
+			{
+				IndexName: aws.String("gs2"),
+				KeySchema: []*dynamodb.KeySchemaElement{
+					{AttributeName: aws.String("pppk"), KeyType: aws.String("HASH")},
+					{AttributeName: aws.String("sk"), KeyType: aws.String("RANGE")},
 				},
 				Projection: &dynamodb.Projection{
 					ProjectionType: aws.String(dynamodb.ProjectionTypeAll),
@@ -110,11 +124,13 @@ func mapMeditationToMeditationRecord(m Meditation) MeditationRecord {
 	pk := m.UserId
 	sk := m.Name + "/" + m.ID
 	ppk := m.ID
+	pppk := strconv.FormatBool(m.Public)
 
 	return MeditationRecord{
 		Pk:         pk,
 		Sk:         sk,
 		Ppk:        ppk,
+		Pppk:       pppk,
 		Meditation: m,
 	}
 }
@@ -172,6 +188,42 @@ func (store DynamoMeditationStore) ListMeditations(userId string) ([]Meditation,
 	return meditations, nil
 }
 
+func (store DynamoMeditationStore) ListPublicMeditations() ([]Meditation, error) {
+
+	params := &dynamodb.QueryInput{
+		TableName:              aws.String(store.tableName),
+		KeyConditionExpression: aws.String("pppk = :isPublic"),
+		IndexName:              aws.String("gs2"),
+		ExpressionAttributeValues: map[string]*dynamodb.AttributeValue{
+			":isPublic": {
+				S: aws.String("true"),
+			},
+		},
+	}
+
+	resp, err := store.svc.Query(params)
+	if err != nil {
+		fmt.Println(err)
+		return []Meditation{}, err
+	}
+
+	var meditationRecords []MeditationRecord
+	err = dynamodbattribute.UnmarshalListOfMaps(resp.Items, &meditationRecords)
+
+	if err != nil {
+		return []Meditation{}, err
+	}
+
+	meditations := make([]Meditation, len(meditationRecords))
+	for i, m := range meditationRecords {
+		meditations[i] = m.Meditation
+	}
+
+	// meditations := []Meditation{}
+
+	return meditations, nil
+}
+
 func (store DynamoMeditationStore) GetMeditation(userId string, id string) (Meditation, error) {
 	params := &dynamodb.QueryInput{
 		TableName:              aws.String(store.tableName),
@@ -218,8 +270,7 @@ func (store DynamoMeditationStore) DeleteMeditation(userId string, id string) er
 			},
 		},
 	}
-	resp, err := store.svc.DeleteItem(params)
-	fmt.Println(resp)
+	_, err = store.svc.DeleteItem(params)
 	if err != nil {
 		return err
 	}
--- ddb_store_test.go
+++ ddb_store_test.go
@@ -27,6 +27,7 @@ func TestDynamoMeditationStore(t *testing.T) {
 			UserId: "alex",
 			ID:     uuid.NewV4().String(),
 			Name:   "Test Meditation",
+			Public: false,
 		}
 
 		err := store.SaveMeditation(m)
@@ -46,6 +47,7 @@ func TestDynamoMeditationStore(t *testing.T) {
 			UserId: userId,
 			ID:     uuid,
 			Name:   "Test Meditation",
+			Public: false,
 		}
 
 		store.SaveMeditation(m)
@@ -70,6 +72,7 @@ func TestDynamoMeditationStore(t *testing.T) {
 				UserId: userId,
 				Name:   "Meditation " + strconv.Itoa(i),
 				ID:     strconv.Itoa(i),
+				Public: false,
 			})
 		}
 		meditations, err := store.ListMeditations(userId)
@@ -93,6 +96,7 @@ func TestDynamoMeditationStore(t *testing.T) {
 				UserId: userId,
 				Name:   "Meditation " + strconv.Itoa(i),
 				ID:     strconv.Itoa(i),
+				Public: false,
 			})
 		}
 
@@ -115,5 +119,76 @@ func TestDynamoMeditationStore(t *testing.T) {
 			fmt.Println(meditations)
 			t.Errorf("Could not find meditation after updating it")
 		}
+	})
+
+	t.Run("Test Delete", func(t *testing.T) {
+		tableName := uuid.NewV4().String()
+		store := NewDynamoMeditationStore(tableName, true, true)
+
+		userId := "alex"
+
+		numMeditations := 3
+		for i := 0; i < numMeditations; i++ {
+			store.SaveMeditation(Meditation{
+				UserId: userId,
+				Name:   "Meditation " + strconv.Itoa(i),
+				ID:     strconv.Itoa(i),
+				Public: false,
+			})
+		}
+
+		m, err := store.GetMeditation(userId, "0")
+		if err != nil {
+			t.Error("Did not find meditation with ID 0")
+		}
+		store.DeleteMeditation(userId, "0")
+
+		meditations, err := store.ListMeditations(userId)
+		if err != nil {
+			t.Error("Couldn't list medtations for userId " + userId)
+		}
+		if len(meditations) != numMeditations-1 {
+			t.Errorf("Found %d meditations, Expected %d meditations", len(meditations), numMeditations)
+		}
+		if contains(meditations, m) {
+			t.Errorf("Found meditation after deleting it!")
+		}
+	})
+
+	t.Run("Test Public Meditations", func(t *testing.T) {
+		tableName := uuid.NewV4().String()
+		store := NewDynamoMeditationStore(tableName, true, true)
+
+		users := []string{"gregory", "alexandria", "maximus"}
+
+		numMeditations := 3
+		for _, user := range users {
+			for i := 0; i < numMeditations; i++ {
+				store.SaveMeditation(Meditation{
+					UserId: user,
+					Name:   "Meditation Private " + strconv.Itoa(i),
+					ID:     strconv.Itoa(i),
+					Public: false,
+				})
+				store.SaveMeditation(Meditation{
+					UserId: user,
+					Name:   "Meditation Public " + strconv.Itoa(i),
+					ID:     strconv.Itoa(i * 10),
+					Public: true,
+				})
+			}
+		}
+
+		expectedPublicMeditations := len(users) * numMeditations
+		publicMeditations, err := store.ListPublicMeditations()
+
+		if err != nil {
+			t.Error("Problem listing public meditations")
+			t.Error(err.Error())
+		}
+		if len(publicMeditations) != expectedPublicMeditations {
+			t.Errorf("Expected %d meditations, got %d meditations", expectedPublicMeditations, len(publicMeditations))
+		}
+
 	})
 }
