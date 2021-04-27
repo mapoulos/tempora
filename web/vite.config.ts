097794f1f10ba324315571ae205f88548631bc61
Alex Poulos <alex@equul.us>
2021-04-27 12:05:29 -0400

    test redirects (#32)
    * test redirects
    
    * fixing vite config to not wipe _redirects

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -3,6 +3,9 @@ import reactRefresh from '@vitejs/plugin-react-refresh'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    emptyOutDir: false
+  },
   plugins: [reactRefresh()],
   resolve: {
     alias: [
