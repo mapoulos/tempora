a155440a474073dde4564ef221403f0dd23b4da8
Alex Poulos <alex.poulos@washpost.com>
2021-05-07 10:37:09 -0400

    updating the form page for meditations, will now copy for sequences...

--- web/src/features/meditation/CreateUpdateMeditation.tsx
+++ web/src/features/meditation/CreateUpdateMeditation.tsx
@@ -12,21 +12,28 @@ import {
   CardActions,
   FormControlLabel,
   Checkbox,
+  Typography,
+  Divider,
+  Tooltip,
 } from "@material-ui/core";
+import { Cancel, Delete } from "@material-ui/icons";
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory, useParams } from "react-router";
 import { AppDispatch } from "../../app/store";
 import { selectIdToken } from "../user/userSlice";
 import { CreateMeditationInput, uploadMp3 } from "./meditationService";
-import { createMeditationThunk, selectPrivateMeditations, setCurrentMeditation, updateMeditationThunk } from "./meditationSlice";
+import {
+  createMeditationThunk,
+  selectPrivateMeditations,
+  setCurrentMeditation,
+  updateMeditationThunk,
+} from "./meditationSlice";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     formRoot: {
       margin: theme.spacing(1),
-      // width: "25ch",
-      // flexGrow: 1,
       display: "flex",
     },
     gridContainer: {
@@ -34,7 +41,7 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     gridItem: {
       flexGrow: 1,
-      spacing: theme.spacing(1)
+      spacing: theme.spacing(1),
     },
     nameField: {
       flexGrow: 1,
@@ -47,15 +54,11 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     toolbar: theme.mixins.toolbar,
     fileSelectRow: {
-      // flexGrow: 1,
-      // width: 400,
-      // justifyContent: "flex-end",
       paddingBottom: theme.spacing(2),
+      paddingTop: theme.spacing(2),
     },
     selectFileTextField: {
-      // flexGrow: 1,
       paddingBottom: theme.spacing(2),
-      // overflow: "hidden",
     },
     submitButtonRow: {
       justifyContent: "flex-end",
@@ -75,28 +78,31 @@ export interface CreateOrUpdateMeditationProps {
 }
 
 export const CreateOrUpdateMeditation = () => {
-  const dispatch = useDispatch<AppDispatch>()
-  const idToken = useSelector(selectIdToken)
-  const privateMeditations = useSelector(selectPrivateMeditations)
-  const history = useHistory()
+  const dispatch = useDispatch<AppDispatch>();
+  const idToken = useSelector(selectIdToken);
+  const privateMeditations = useSelector(selectPrivateMeditations);
+  const history = useHistory();
   const [state, setState] = useState({
-	  audioFile: {} as File,
+    audioFile: {} as File,
     meditation: {
       name: "",
       text: "",
       isPublic: false,
-      uploadKey: ""
+      uploadKey: "",
     },
-  })
+  });
 
-  const {meditationId} = useParams<CreateOrUpdateMeditationProps>()
+  const { meditationId } = useParams<CreateOrUpdateMeditationProps>();
 
-  const meditation = (meditationId !== undefined) ? privateMeditations.find((m) => m._id === meditationId) : undefined
+  const meditation =
+    meditationId !== undefined
+      ? privateMeditations.find((m) => m._id === meditationId)
+      : undefined;
 
-  const isUpdate = meditation !== undefined
+  const isUpdate = meditation !== undefined;
 
   useEffect(() => {
-    if(isUpdate) {
+    if (isUpdate) {
       setState({
         ...state,
         meditation: {
@@ -104,17 +110,20 @@ export const CreateOrUpdateMeditation = () => {
           name: meditation?.name ?? "",
           text: meditation?.text ?? "",
           isPublic: meditation?.isPublic ?? false,
-        }
-      })
+        },
+      });
     }
-  }, [])
+  }, []);
 
   const isTextFieldValid = (val: string) => {
-    const trimmed = val.trim()
-    return trimmed.length > 0 && trimmed.length < 2000
-  }
+    const trimmed = val.trim();
+    return trimmed.length > 0 && trimmed.length < 2000;
+  };
 
-  const isSubmitEnabled = isTextFieldValid(state.meditation.name) && isTextFieldValid(state.meditation.text) && (isTextFieldValid(state.meditation.uploadKey) || isUpdate)
+  const isSubmitEnabled =
+    isTextFieldValid(state.meditation.name) &&
+    isTextFieldValid(state.meditation.text) &&
+    (isTextFieldValid(state.meditation.uploadKey) || isUpdate);
   const classes = useStyles();
 
   const handleNameFieldChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
@@ -122,74 +131,99 @@ export const CreateOrUpdateMeditation = () => {
       ...state,
       meditation: {
         ...state.meditation,
-        name: evt.target.value || ""
-      }
-    })
-  }
+        name: evt.target.value || "",
+      },
+    });
+  };
   const handleTextFieldChange = (evt: React.ChangeEvent<HTMLInputElement>) => {
     setState({
       ...state,
       meditation: {
         ...state.meditation,
-        text: evt.target.value || ""
-      }
-    })
-  }
+        text: evt.target.value || "",
+      },
+    });
+  };
 
-
-
-  const handleFileSelection = async (evt: React.ChangeEvent<HTMLInputElement>) => {
-    const files = evt.target?.files ?? []
-    const file = files[0]
+  const handleFileSelection = async (
+    evt: React.ChangeEvent<HTMLInputElement>
+  ) => {
+    const files = evt.target?.files ?? [];
+    const file = files[0];
 
     if (idToken) {
-      const key = await uploadMp3(file, idToken)
+      const key = await uploadMp3(file, idToken);
       setState({
-		  ...state,
-		  audioFile: file,
-		  meditation:{
-			  ...state.meditation,
-			  uploadKey: key
-		  }
-	  })
+        ...state,
+        audioFile: file,
+        meditation: {
+          ...state.meditation,
+          uploadKey: key,
+        },
+      });
     }
   };
 
-  const handleIsPublicSelection = (evt: React.ChangeEvent<HTMLInputElement>) => {
+  const handleIsPublicSelection = (
+    evt: React.ChangeEvent<HTMLInputElement>
+  ) => {
     setState({
       ...state,
       meditation: {
         ...state.meditation,
-        isPublic: evt.target.checked
-      }
-    })
+        isPublic: evt.target.checked,
+      },
+    });
   };
 
   const handleSubmit = async () => {
-    const createOrUpdateMeditationArgs: CreateMeditationInput = state.meditation
+    const createOrUpdateMeditationArgs: CreateMeditationInput =
+      state.meditation;
     try {
       if (isUpdate) {
-        await dispatch(updateMeditationThunk({
-          ...createOrUpdateMeditationArgs,
-          _id: meditationId || "MEDITATION_ID_NOT_FOUND"
-        }, idToken as IdToken))
-        history.push("/private-meditations")
+        await dispatch(
+          updateMeditationThunk(
+            {
+              ...createOrUpdateMeditationArgs,
+              _id: meditationId || "MEDITATION_ID_NOT_FOUND",
+            },
+            idToken as IdToken
+          )
+        );
+        history.push("/private-meditations");
       } else {
-        const newMeditation = await dispatch(createMeditationThunk(createOrUpdateMeditationArgs, idToken as IdToken))
-        dispatch(setCurrentMeditation(newMeditation))
-        history.push("/")
+        const newMeditation = await dispatch(
+          createMeditationThunk(
+            createOrUpdateMeditationArgs,
+            idToken as IdToken
+          )
+        );
+        dispatch(setCurrentMeditation(newMeditation));
+        history.push("/");
       }
-    } catch(error) {
-      console.error("There was a problem creating or updating the meditation")
-      console.error(error)
+    } catch (error) {
+      console.error("There was a problem creating or updating the meditation");
+      console.error(error);
     }
+  };
+
+  const resetChooseMedia = () => {
+    setState({
+      audioFile: {} as File,
+      meditation: {
+        ...state.meditation,
+        uploadKey: ""
+      }
+    })
   }
 
   return (
     <React.Fragment>
       <div className={classes.toolbar} />
       <Card>
-        <CardHeader title={isUpdate ? "Update Meditation" : "Create Meditation"} />
+        <CardHeader
+          title={isUpdate ? "Update Meditation" : "Create Meditation"}
+        />
         <CardContent>
           <form className={classes.formRoot} autoComplete="off">
             <Grid container className={classes.gridContainer}>
@@ -221,43 +255,76 @@ export const CreateOrUpdateMeditation = () => {
                   value={state.meditation.text}
                 ></TextField>
               </Grid>
-              <Grid item className={classes.gridItem} xs={12}>
-                <FormControlLabel control={
-                  <Checkbox checked={state.meditation.isPublic} onChange={handleIsPublicSelection} />
-                } label="Make Public?"  labelPlacement="start"/>
+              <Grid container direction="row" justify="flex-start">
+                <Grid item className={classes.fileSelectRow} xs={12}>
+                  {state.meditation.uploadKey === "" ? (
+                    <Grid container>
+                      <Grid item>
+                    <Button
+                      style={{ height: "100%" }}
+                      size="large"
+                      component="label"
+                      variant="contained"
+                      fullWidth
+                    >
+                      Choose{" "}
+                      {isUpdate ? (
+                        <div>&nbsp;New&nbsp;</div>
+                      ) : (
+                        <div>&nbsp;</div>
+                      )}{" "}
+                      MP3
+                      <input
+                        type="file"
+                        hidden
+                        accept=".mp3"
+                        onChange={handleFileSelection}
+                      ></input>
+                    </Button>
+                    </Grid>
+                    </Grid>
+                  ) : (
+                    <Grid container direction="row">
+                      <Grid item xs={12}>
+                        <TextField
+                          variant="outlined"
+                          InputProps={{ readOnly: true,
+                            endAdornment: <Button size="small" onClick={() => {resetChooseMedia()}}><Cancel /></Button>
+                          }}
+                          fullWidth
+                          value={state.audioFile.name}
+                        />
+                      </Grid>
+                    </Grid>
+                  )}
+                </Grid>
               </Grid>
-              <Grid container direction="row" justify="flex-end">
-                <Grid item className={classes.fileSelectRow} xs={6} md={3}>
-                  <Button
-                    style={{ height: "100%"}}
-                    size="large"
-                    component="label"
-                    variant="contained"
-                    fullWidth
-                  >
-                    Choose {isUpdate ? (<div>&nbsp;New&nbsp;</div>) : (<div>&nbsp;</div>)} MP3
-                    <input
-                      type="file"
-                      hidden
-                      accept=".mp3"
-                      onChange={handleFileSelection}
-                    ></input>
-                  </Button>
-                </Grid>
-                <Grid item className={classes.fileSelectRow} xs={6} md={9}>
-                  <TextField
-                    variant="outlined"
-                    label={state.audioFile?.name ?? "" }
-                    InputProps={{ readOnly: true }}
-                    fullWidth={true}
-                  />
-                </Grid>
+              <Grid item className={classes.gridItem} xs={12}>
+                <Tooltip title={<Typography>Published meditations are discoverable by all users of Tempora.</Typography>}>
+                <FormControlLabel
+                  control={
+                    <Checkbox
+                      checked={state.meditation.isPublic}
+                      size="medium"
+                      onChange={handleIsPublicSelection}
+                    />
+                  }
+                  label="Published"
+                  labelPlacement="end"
+                />
+                </Tooltip>
               </Grid>
             </Grid>
           </form>
         </CardContent>
         <CardActions className={classes.cardFooter}>
-          <Button disabled={!isSubmitEnabled} size="large" variant="outlined" style={{ height: 50 }} onClick={handleSubmit}>
+          <Button
+            disabled={!isSubmitEnabled}
+            size="large"
+            variant="outlined"
+            style={{ height: 50 }}
+            onClick={handleSubmit}
+          >
             Save
           </Button>
         </CardActions>
